fc51e14d245f6fa32b0de7060a8f7f5e2831b0b3
haruki7049 <tontonkirikiri@gmail.com>
2025-05-13 17:53:15 +0900

    style: nix fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 pub fn run(str: String, target: &str) -> anyhow::Result<String> {
     let mut result: String = String::new();
     let sep_str: Vec<&str> = str.split('\n').collect(); // Separated String at '\n'
-    
+
     for s in sep_str {
         if s.contains(target) {
             result.push_str(s);

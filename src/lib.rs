efd2cdd0c4a8ab537d7c80edf731b7f79507b8c7
haruki7049 <tontonkirikiri@gmail.com>
2025-05-16 10:08:49 +0900

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-pub fn run(str: String, target: &str) -> anyhow::Result<String> {
+use hrtor::processor::HrtorProcessor;
+
+pub fn run(_processor: HrtorProcessor, str: String, target: &str) -> anyhow::Result<String> {
     let mut result: String = String::new();
     let sep_str: Vec<&str> = str.split('\n').collect(); // Separated String at '\n'
 
